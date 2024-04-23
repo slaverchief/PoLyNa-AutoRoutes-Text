9cf680d996154d30e1868f4142a41c9cf590fa61
slavechief <uvaysov.2005@mail.ru>
2024-04-23 23:46:44 +0300

    StageFix

--- route_generation.py
+++ route_generation.py
@@ -31,13 +31,30 @@ class RouteMaker:
         return count
 
 
+    def __format_route(self, l):
+        is_cross_staging = False
+        i = 0
+        while i != len(l):
+            spl1 = l[i].split('-')
+            if len(spl1) == 4 and spl1[2] == 'stair':
+                if not is_cross_staging:
+                    is_cross_staging = True
+                else:
+                    spl2 = l[i+1].split('-')
+                    if len(spl2) == 4 and spl2[2] == 'stair':
+                        del l[i]
+                        continue
+                    else:
+                        is_cross_staging = False
+            i += 1
+        return l
 
 
     def __get_route(self, from_point: str, to_point: str):
 
         command = f'return graph.getShortestWayFromTo("{from_point}","{to_point}")'
         res = self.__driver.execute_script(command)
-        return res['way']
+        return self.__format_route(res['way'])
 
     def __waiting_for_the_turn(self, l):
         count = 0
@@ -54,6 +71,11 @@ class RouteMaker:
     def __generate_str(self, route_list, to_p):
         string = ''
         stages_history = []
+
+        def skip_a_turn(node):
+            if (node in ('tl', 'tr') or (node.split('_') and  node.split('_')[0] == 'st')):
+                return False
+            return True
         for i in range(len(route_list)):
             move = route_list[i]
             if move == 'tl':
@@ -71,13 +93,13 @@ class RouteMaker:
                 if self.__waiting_for_the_turn(route_list[i:]):
                     string += 'до следующего поворота, '
             elif move == 'gfs':
-                if not route_list[i+1] in ('tl', 'tr'):
+                if skip_a_turn(route_list[i+1]):
                     string += 'пропустите поворот, '
             elif move != 'gf':
                 spl = move.split('_')
                 if not stages_history or len(stages_history)%2 == 0:
                     if spl[0] == 'st':
-                        string += 'идите на лестницу и '
+                        string += 'до лестницы и'
                 stages_history.append(int(spl[1]))
                 if len(stages_history) > 1 and len(stages_history)%2 == 0:
                     if stages_history[-1] > stages_history[-2]:
